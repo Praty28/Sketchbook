89cb0611ed5394f486e8e70ab5989f76b776f687
Pratyush Mendon <mendonpratyush908@gmail.com>
2021-06-22 14:12:54 +0530

    Add a for loop with event listener to add a class to divs when mouse hovered

--- script.js
+++ script.js
@@ -12,4 +12,21 @@ addDivs(16);
 const divs = document.querySelectorAll('div:not(#parentDiv):not(#container)');
 for (i=0; i < divs.length; i++) {
  divs[i].classList.add('blue');
+
 }
+
+
+
+for (let e=0; e < divs.length; e++) {
+    
+    divs[e].addEventListener('mouseover', getIndex);
+  
+    var getIndex = function () {
+        divs[e].classList.add('pink');
+        console.log(e);
+    }
+}
+
+
+
+
